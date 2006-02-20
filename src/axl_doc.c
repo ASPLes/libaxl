368263c940aceefff038b308c7bf627857f8e02a
Francis Brosnan Blázquez <francis@aspl.es>
2006-02-20 17:27:47 +0000

    libaxl: * [fix] making axl_stream_get_until function to report which chunk have been matched.
    * [new] adding more internal api to report where we are while reading the stream:
      - axl_stream_get_following
    
    * [fix] fixing axl_doc_parse_node to be able to parse axl nodes that uses the > and />
      form but really close to the xml node name. Starting to parse serious xml chunks.
    
    * [fix] making axl error module to also report the stream header being parsed once
      the error is reported.(using previous function axl_stream_get_following).

--- src/axl_doc.c
+++ src/axl_doc.c
@@ -389,7 +389,7 @@ bool __axl_doc_parse_xml_header (axlStream * stream, axlDoc * doc, axlError ** e
 			axl_log (LOG_DOMAIN, AXL_LEVEL_DEBUG, "found encoding declaration");
 
 			/* found encoding instruction */
-			string_aux = axl_stream_get_until (stream, NULL, 2, "'", "\"");
+			string_aux = axl_stream_get_until (stream, NULL, NULL, 2, "'", "\"");
 			if (string_aux == NULL) {
 				axl_error_new (-2, "expected encoding value, not found.", stream, error);
 				axl_stream_free (stream);
@@ -409,7 +409,7 @@ bool __axl_doc_parse_xml_header (axlStream * stream, axlDoc * doc, axlError ** e
 		if ((axl_stream_inspect_several (stream, 2, "standalone=\"", "standalone='") > 0)) {
 			
 			/* found standalone instruction */
-			string_aux = axl_stream_get_until (stream, NULL, 2, "'", "\"");
+			string_aux = axl_stream_get_until (stream, NULL, NULL, 2, "'", "\"");
 			if (string_aux == NULL) {
 				axl_error_new (-2, "expected to receive standalone value, not found.", stream, error);
 				axl_stream_free (stream);
@@ -481,6 +481,7 @@ bool __axl_doc_parse_node (axlStream * stream, axlDoc * doc, axlNode ** calling_
 {
 	char    * string_aux;
 	axlNode * node;
+	int       matched_chunk;
 	
 	axl_return_val_if_fail (stream, AXL_FALSE);
 	axl_return_val_if_fail (doc, AXL_FALSE);
@@ -492,7 +493,8 @@ bool __axl_doc_parse_node (axlStream * stream, axlDoc * doc, axlNode ** calling_
 	if (! (axl_stream_inspect (stream, "<") > 0) && ! axl_stream_remains (stream)) {
 		/* check if we are reading the first node node */
 		if (doc->rootNode == NULL)
-			axl_error_new (-2, "expected initial < for a root node definition, not found. An xml document must have, at least, one node definition.", stream, error);
+			axl_error_new (-2, "expected initial < for a root node definition, not found. An xml document must have, at least, one node definition.", 
+				       stream, error);
 		else
 			axl_error_new (-2, "expected initial < for a node definition, not found.", stream, error);
 		axl_stream_free (stream);
@@ -500,7 +502,7 @@ bool __axl_doc_parse_node (axlStream * stream, axlDoc * doc, axlNode ** calling_
 	}
 
 	/* get node name */
-	string_aux = axl_stream_get_until (stream, NULL, 3, " ", "/", ">");
+	string_aux = axl_stream_get_until (stream, NULL, &matched_chunk, 5, " />", "/>", ">", " >", " ");
 	if (AXL_IS_STR_EMPTY (string_aux)) {
 		axl_error_new (-2, "expected an non empty content for the node name not found.", stream, error);
 		axl_stream_free (stream);
@@ -509,9 +511,11 @@ bool __axl_doc_parse_node (axlStream * stream, axlDoc * doc, axlNode ** calling_
 
 	/* create the node and associate it */
 	node           = axl_node_create (string_aux);
-	if (doc->rootNode == NULL)
+	if (doc->rootNode == NULL) {
+		axl_log (LOG_DOMAIN, AXL_LEVEL_DEBUG, "setting as first node found, the root node: <%s>", string_aux);
 		doc->rootNode  = node;
-	else {
+		axl_stack_push (doc->parentNode, node);
+	} else {
 		/* or set the node as a child of the current parent */
 		axl_doc_set_child_current_parent (doc, node);
 	}
@@ -521,30 +525,51 @@ bool __axl_doc_parse_node (axlStream * stream, axlDoc * doc, axlNode ** calling_
 	if (calling_node != NULL)
 		*calling_node = node;
 
+	axl_log (LOG_DOMAIN, AXL_LEVEL_DEBUG, "node found: %s", string_aux);
+
 	/* know, until the node ends, we have to find the node
 	 * attributes or the node defintion end */
 	while (axl_stream_remains (stream)) {
+
 		/* get rid from spaces */
 		AXL_CONSUME_SPACES (stream);
 		
 		/* check if we have an attribute for the node, or the node
 		 * definition have ended or the node definition is an empty
-		 * one */
-		if (axl_stream_inspect (stream, "/>")) {
+		 * one 
+		 * 
+		 * the following code that relies on matched_chunk is
+		 * done due to previous call to get_until function. If
+		 * the value 0 or 1 was matched, this means that we
+		 * are on "/>" case */
+		if ((matched_chunk == 0) ||
+		    (matched_chunk == 1) ||
+		    axl_stream_inspect_several (stream, 2, "/>", " />") > 0) {
+			axl_log (LOG_DOMAIN, AXL_LEVEL_DEBUG, "found end xml node definition '/>'");
 			/* empty node configuration found */
 			axl_node_set_is_empty (node, AXL_TRUE);
 			axl_node_set_have_childs (node, AXL_FALSE);
 
 			/* make this node to be complated and no child could be set. */
 			axl_doc_pop_current_parent (doc);
-			break;
+			return AXL_TRUE;
 		}
 		
-		/* found node definition termination */
-		if (axl_stream_inspect (stream, ">")) {
+		/* check if we have an attribute for the node, or the node
+		 * definition have ended or the node definition is an empty
+		 * one 
+		 * 
+		 * the following code that relies on matched_chunk is
+		 * done due to previous call to get_until function. If
+		 * the value 2 or 3 was matched, this means that we
+		 * are on ">" case */
+		if ((matched_chunk == 2) ||
+		    (matched_chunk == 3) ||
+		    axl_stream_inspect_several (stream, 2, ">", " >") > 0) {
+			axl_log (LOG_DOMAIN, AXL_LEVEL_DEBUG, "found end xml node definition '>'");
 			axl_node_set_have_childs (node, AXL_TRUE);
 			/* this node is ended */
-			break;
+			return AXL_TRUE;
 		}
 		
 		/* found attribute declaration, try to read it */
@@ -561,7 +586,7 @@ bool __axl_doc_parse_node (axlStream * stream, axlDoc * doc, axlNode ** calling_
  */
 bool __axl_doc_parse_close_node (axlStream * stream, axlDoc * doc, axlNode ** node, axlError ** error)
 {
-
+	return AXL_FALSE;
 }
 
 
@@ -581,7 +606,11 @@ bool __axl_doc_parse_close_node (axlStream * stream, axlDoc * doc, axlNode ** no
  * 
  * @param entity The XML document to load.
  *
- * @param entity_size The XML document size to load.
+ * @param entity_size The XML document size to load. If a <b>-1</b> is
+ * provided, strlen function is used to figure out current document
+ * size. This is not recomended while using xml documents that include
+ * binary data, that maybe comes inside the CDATA section or because
+ * an utf caracter used that includes the \0 inside its value.
  * 
  * @return A newly allocated Axl Document, that must be deallocated
  * using \ref axl_doc_free, when no longer needed. The function could
@@ -594,10 +623,10 @@ bool __axl_doc_parse_close_node (axlStream * stream, axlDoc * doc, axlNode ** no
  */
 axlDoc * axl_doc_parse (char * entity, int entity_size, axlError ** error)
 {
-	char      * string;
-	axlStream * stream = NULL;
-	axlDoc    * doc    = NULL;
-	axlNode   * node   = NULL;
+	axlStream * stream    = NULL;
+	axlDoc    * doc       = NULL;
+	axlNode   * node      = NULL;
+	int         iterator  = 0;
 		
 	
 	/* check for environmental parameters */
@@ -606,14 +635,17 @@ axlDoc * axl_doc_parse (char * entity, int entity_size, axlError ** error)
 		return NULL;
 	}
 
-	if (entity_size <= 0) {
-		axl_error_new (-1, "Received an entity size that is less or equal to 0.", stream, error);
+	if ((entity_size != -1) && (entity_size <= 0)) {
+		axl_error_new (-1, "Received an entity size that is less or equal to 0, and it is not -1.", stream, error);
 		return NULL;
 	}
 
 	/* create the xml stream using provided data */
-	stream = axl_stream_new (entity, entity_size);
-	doc    = axl_new (axlDoc, 1);
+	if (entity_size == -1)
+		stream = axl_stream_new (entity, strlen (entity));
+	else
+		stream = axl_stream_new (entity, entity_size);
+	doc            = axl_new (axlDoc, 1);
 	axl_stream_link (stream, doc);
 
 	/* parse initial xml header */
@@ -627,6 +659,8 @@ axlDoc * axl_doc_parse (char * entity, int entity_size, axlError ** error)
 	/* if the node returned is not empty */
 	if (! axl_node_is_empty (node)) {
 
+		axl_log (LOG_DOMAIN, AXL_LEVEL_DEBUG, "the first node ready, have content, reading it");
+
 		/* while the stream have data */
 		while (axl_stream_remains (stream)) {
 			
@@ -634,6 +668,7 @@ axlDoc * axl_doc_parse (char * entity, int entity_size, axlError ** error)
 			AXL_CONSUME_SPACES (stream);
 
 			if (axl_stream_peek (stream, "</") > 0) {
+				axl_log (LOG_DOMAIN, AXL_LEVEL_DEBUG, "found a node termination signal");
 				/* seems that a node is being closed */
 				if (! __axl_doc_parse_close_node (stream, doc, &node, error))
 					return NULL;
@@ -641,13 +676,20 @@ axlDoc * axl_doc_parse (char * entity, int entity_size, axlError ** error)
 			}
 
 			if (axl_stream_peek (stream, "<") > 0) {
+				axl_log (LOG_DOMAIN, AXL_LEVEL_DEBUG, "found a new node being opened");
 				/* seems that another node is being opened */
 				if (!__axl_doc_parse_node (stream, doc, &node, error))
 					return NULL;
 				continue;
 			}
-			
 
+			/* found node content */
+			axl_log (LOG_DOMAIN, AXL_LEVEL_DEBUG, "found a node content");
+			if (iterator == 3) {
+				axl_error_new (-1, "not implemented xml node content parsing..", stream, error);
+				return NULL;
+			}
+			iterator++;
 		}
 	}
 
@@ -656,6 +698,8 @@ axlDoc * axl_doc_parse (char * entity, int entity_size, axlError ** error)
 	axl_stream_free (stream);
 	return doc;
 }
+
+
 
 /** 
  * @brief Gets current axl Document encoding.
