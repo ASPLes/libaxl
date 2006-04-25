c42d2501bb79b5b21186ce510c3a8a627be5444c
Francis Brosnan Blázquez <francis@aspl.es>
2006-04-25 10:18:23 +0000

    libaxl: * [fix] fixing a bug that was causing to not properly set the repetition pattern for   top level DTD element definitions, that was nested by others. Added a regression   test to ensure not fall again.
    * [new] added support to perform DTD element validation for sequence element that 
      have a repetition pattern one or many (+). Added a regression test to ensure not
      fall on this again.
    
    * [new] adding a new log macro __axl_log which removes/enables axl_log declarations
      according to the log debug configuration.
    
    * [fix] removed -pendantic option from src/Makefile.am.

--- src/axl_log.h
+++ src/axl_log.h
@@ -51,6 +51,13 @@ bool axl_log_is_enabled ();
 
 bool axl_log_color_is_enabled ();
 
+
+#ifdef SHOW_DEBUG_LOG
+#define __axl_log(domain, level, message, ...)  axl_log(domain, level, message, ##__VA_ARGS__)
+#else
+/* nothing */
+#endif
+
 void axl_log (char * domain, AxlDebugLevel level, char * message, ...);
 
 /* @} */
