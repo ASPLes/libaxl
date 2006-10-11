94509e3d502966a65cea2f06ed09cdc30228e742
Francis Brosnan Blázquez <francis@aspl.es>
2006-10-11 08:09:13 +0000

    libaxl: * [fix] Commiting axl_config_win32.h file (specific to the win32 platform)   and Makefile.am updated to include this file into the extra dist.

---
+++ src/axl_config_win32.h
@@ -0,0 +1,55 @@
+/*
+ * Axl Library axl_config.h
+ * Platform dependant definitions for Win32 platform.
+ *
+ * This file is maintained manually for those people that do not
+ * compile axl using autoconf. It should look really similar to
+ * axl_config.h file created on a i386 linux platform but changing
+ * AXL_OS_UNIX to AXL_OS_WIN32 (at least for now).
+ *
+ */
+
+#ifndef __AXL_CONFIG_H__
+#define __AXL_CONFIG_H__
+
+/**
+ * \addtogroup axl_decl_module
+ * @{
+ */
+
+/**
+ * @brief Allows to convert integer value (including constant values)
+ * into a pointer representation.
+ *
+ * Use the oposite function to restore the value from a pointer to a
+ * integer: \ref PTR_TO_INT.
+ *
+ * @param integer The integer value to cast to pointer.
+ *
+ * @return A \ref axlPointer reference.
+ */
+#define INT_TO_PTR(integer)   ((axlPointer)  integer)
+
+/**
+ * @brief Allows to convert a pointer reference (\ref axlPointer),
+ * which stores an integer that was stored using \ref INT_TO_PTR.
+ *
+ * Use the oposite function to restore the pointer value stored in the
+ * integer value.
+ *
+ * @param ptr The pointer to cast to a integer value.
+ *
+ * @return A int value.
+ */
+#define PTR_TO_INT(ptr) ((int)  ptr)
+
+/**
+ * @brief Allows to get current platform configuration. This is used by Axl library
+ * but could be used by applications built on top of Axl to change its configuration
+ * based on the platform information.
+ */
+#define AXL_OS_WIN32
+
+/* @} */
+
+#endif
