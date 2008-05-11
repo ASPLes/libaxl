619fac6947c798ddbc1158ea542a6508f166053e
Francis Brosnan Blázquez <francis@aspl.es>
2008-05-11 14:41:54 +0000

    libaxl: * [new] Added support to babel for iso-8859-4 encoding.

--- babel/axl_babel.c
+++ babel/axl_babel.c
@@ -55,8 +55,11 @@
  * library to use its API to support encoding formats.
  *
  * Current encoding format supported at:
+ *
  * - utf-8, ascii.
- * - iso-8859-1, iso-8859-2, iso-8859-3, iso-8859-9, iso-8859-15
+ *
+ * - iso-8859-1, iso-8859-2, iso-8859-3, iso-8859-4, iso-8859-9,
+ * iso-8859-15
  * 
  * @param error An optional reference to an axlError where failure
  * will be notified.
@@ -367,6 +370,12 @@ bool axl_babel_configure_encoding (axlStream  * stream,
 		/* install a translator handler */
 		__axl_log (LOG_DOMAIN, AXL_LEVEL_DEBUG, "installed handler encoding for iso-8859-3");
 		table = axl_babel_build_iso88593_table ();
+	} /* end if */
+
+	if (axl_cmp (encoding, "iso88594")) {
+		/* install a translator handler */
+		__axl_log (LOG_DOMAIN, AXL_LEVEL_DEBUG, "installed handler encoding for iso-8859-4");
+		table = axl_babel_build_iso88594_table ();
 	} /* end if */
 
 	if (axl_cmp (encoding, "iso88599")) {
