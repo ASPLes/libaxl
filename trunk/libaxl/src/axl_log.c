0b31acc555f3026bfc969f917d097c87d8646ebf
acinom <acinom@9996078b-26e8-0310-a12f-ea49881d8364>
2013-03-08 10:55:26 +0000

    libaxl: * [fix] Last bug was reported by Étienne Dupuis

--- trunk/libaxl/src/axl_log.c
+++ trunk/libaxl/src/axl_log.c
@@ -90,7 +90,7 @@ axl_bool      axl_log_is_enabled (void)
 axl_bool    axl_log_color_is_enabled (void)
 {
 #if defined(AXL_OS_WIN32) && ! defined(__GNUC__)
-	size_t requiredSize;
+	size_t  requiredSize;
 #endif
 
 	if (not_executed_color) {
