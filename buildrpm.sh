d5fba835972a1ba75a00fc505d6df88bc5f5c1e4
Francis Brosnan Blázquez <francis@aspl.es>
2016-08-19 16:31:36 +0200

    libaxl: * [fix] Updated	buildrpm.sh to check error code	and stop failing   reporting that error..

--- buildrpm.sh
+++ buildrpm.sh
@@ -11,6 +11,14 @@ cp axl-`cat VERSION`.tar.gz rpm/SOURCES
 
 echo "Calling to compile packages.."
 LANG=C rpmbuild -ba --define '_topdir /usr/src/libaxl/rpm' rpm/SPECS/libaxl1.spec
+error=$?
+if [ $error != 0 ]; then
+    echo "ERROR: ***"
+    echo "ERROR: rpmbuild command failed, exitcode=$error"
+    echo "ERROR: ***"
+    exit $error
+fi
+
 
 echo "Output ready at rpm/RPMS"
 find rpm/RPMS -type f -name '*.rpm' > rpm/RPMS/files
